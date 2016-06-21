117af7cb1073ecadd13219168a7346a3115eefdf
s8508235 <s8508235@yahoo.com.tw>
2016-06-21 15:30:45 +0800

    add fb with some bug

--- app/src/main/java/netdb/courses/softwarestudio/MainActivity.java
+++ app/src/main/java/netdb/courses/softwarestudio/MainActivity.java
@@ -9,10 +9,40 @@ import android.view.MenuItem;
 import netdb.courses.softwarestudio.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    private RequestQueue mQueue;
+    private final static String mUrl = "http://140.114.206.62/webapp/ServerSendingMsg.php";
+    private TextView msg;
+    private Button mGetMsgButton;
+    private StringRequest getRequest;
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        msg = (TextView) findViewById(R.id.msg);
+        mGetMsgButton = (Button) findViewById(R.id.get_msg);
+        mQueue = Volley.newRequestQueue(this);
+        getRequest = new StringRequest(Request.Method.POST,mUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        msg.setText(s);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        msg.setText(volleyError.getMessage());
+                    }
+                });
+        mGetMsgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQueue.add(getRequest);
+            }
+        });*/
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MainFragment())
--- app/src/main/java/netdb/courses/softwarestudio/fragment/MainFragment.java
+++ app/src/main/java/netdb/courses/softwarestudio/fragment/MainFragment.java
@@ -3,11 +3,28 @@ package netdb.courses.softwarestudio.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+
+import java.util.Arrays;
 
 import netdb.courses.softwarestudio.R;
 import netdb.courses.softwarestudio.SoloActivity;
@@ -17,49 +34,161 @@ import netdb.courses.softwarestudio.TeamActivity;
  * Created by Bill on 2016/6/18.
  */
 public class MainFragment extends Fragment {
+    private TextView mtextView;
+    private ProfilePictureView profilePictureView;
+    CallbackManager callbackManager;
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+    Button btn1,btn2;
+    private FacebookCallback<LoginResult> mcallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            mtextView.setText(constructWelcomeMessage(profile));
+            constructProfileImage(profile);
+        }
 
-    private ArrayAdapter<String> mTeamAdapter;
+        @Override
+        public void onCancel() {
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+        }
+    };
+    public MainFragment() {}
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        callbackManager =CallbackManager.Factory.create();
         // Add this line in order for this fragment to handle menu events.
-        setHasOptionsMenu(false);
+        setupTokenTracker();
+        setupProfileTracker();
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-
-        Button btn1 = (Button) rootView.findViewById(R.id.team_button);
-        Button btn2 = (Button) rootView.findViewById(R.id.solo_button);
-        btn1.setOnClickListener(new Button.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), TeamActivity.class);
-                startActivity(intent);
-            }
-        });
-        btn2.setOnClickListener(new Button.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SoloActivity.class);
-                startActivity(intent);
-            }
-        });
-        return rootView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        btn1 = (Button) view.findViewById(R.id.team_button);
+        btn2 = (Button) view.findViewById(R.id.solo_button);
+        return  view;
     }
-/*
-    @Override
-    protected void onPostExecute(String[] result) {
-        if (result != null) {
-            mTeamAdapter.clear();
-            for(String dayForecastStr : result) {
-                mTeamAdapter.add(dayForecastStr);
-            }
-            // New data is back from the server.  Hooray!
+
+    public void btnlistener()
+    {
+        if(isLoggedIn() == true) {
+            btn1.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), TeamActivity.class);
+                    startActivity(intent);
+                }
+            });
+            btn2.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), SoloActivity.class);
+                    startActivity(intent);
+                }
+            });
         }
-    }*/
+        else
+        {
+            btn1.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(),R.string.remind_login,Toast.LENGTH_SHORT).show();
+                }
+            });
+            btn2.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(),R.string.remind_login,Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState)
+    {
+        setupTextDetails(view);
+        setupLoginButton(view);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        btnlistener();
+        Profile profile = Profile.getCurrentProfile();
+        mtextView.setText(constructWelcomeMessage(profile));
+        constructProfileImage(profile);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.stopTracking();
+        mProfileTracker.stopTracking();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setupTextDetails(View view) {
+        mtextView = (TextView) view.findViewById(R.id.text_details);
+        profilePictureView = (ProfilePictureView) view.findViewById(R.id.profilePicture);
+    }
+
+    private void setupTokenTracker() {
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.d("VIVZ", "" + currentAccessToken);
+            }
+        };
+    }
+    private void setupProfileTracker() {
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.d("VIVZ", "" + currentProfile);
+                mtextView.setText(constructWelcomeMessage(currentProfile));
+                constructProfileImage(currentProfile);
+                onResume();
+            }
+        };
+    }
+
+    private void setupLoginButton(View view) {
+        LoginButton mButtonLogin = (LoginButton) view.findViewById(R.id.login_button);
+        mButtonLogin.setFragment(this);
+        mButtonLogin.setReadPermissions(Arrays.asList("public_profile", "user_friends"));
+        mButtonLogin.registerCallback(callbackManager, mcallback);
+    }
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+    private String constructWelcomeMessage(Profile profile) {
+        StringBuffer stringBuffer = new StringBuffer();
+        if (profile != null) {
+            stringBuffer.append("Welcome " + profile.getName());
+        }
+
+        return stringBuffer.toString();
+    }
+    private void constructProfileImage(Profile profile)
+    {
+        if(profile == null)
+        {
+            profilePictureView.setProfileId(null);;
+            return ;
+        }
+        profilePictureView.setProfileId(profile.getId());
+    }
 }
